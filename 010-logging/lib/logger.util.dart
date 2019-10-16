f532ec011872d6db7f10aa88d8eb0f57b008d501
Tristan <tristan.schuller@coodoo.de>
2019-10-16 11:22:39 +0200

    Beispiel Angepasst.

--- 010-logging/lib/logger.util.dart
+++ 010-logging/lib/logger.util.dart
@@ -1,11 +1,9 @@
 import 'package:logger/logger.dart';
 
 Logger getLogger() {
-  // Farbdarstellung funktioniert leider noch nicht für den iOS Simulator.
-  // Bis dahin deaktiviert.
   return Logger(
     printer: PrettyPrinter(
-      lineLength: 160,
+      lineLength: 90,
       colors: false,
       methodCount: 1,
       errorMethodCount: 5,
--- 010-logging/lib/main.dart
+++ 010-logging/lib/main.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
-
 import 'package:logger/logger.dart';
 import 'package:logging_with_flutter/logger.util.dart';
 
 void main() {
-  // Logger: Wählen welches Level angezeigt werden soll.
   Logger.level = Level.debug;
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // Logger: In der jeweiligen Klasse erstellen um den Logger nutzen zu können.
   final log = getLogger();
 
   @override
   Widget build(BuildContext context) {
+    log.d('Debug-Nachricht aus der build-Methode.');
+    log.i('Info-Nachricht aus der build-Methode.');
+    log.e('Error-Nachricht aus der build-Methode.');
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Logger: Log-Nachricht einfügen.
-            log.i('Nachricht mit Level: Info.');
-          },
-        ),
+        backgroundColor: Colors.white,
         body: Center(
-          child: Text("Lorem ipsum."),
-        ),
+            child: FlatButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          onPressed: exampleMethod,
+          child: Text(
+            "Log-Nachricht ausgeben.",
+            style: TextStyle(fontSize: 20.0),
+          ),
+        )),
       ),
     );
+  }
+
+  exampleMethod() {
+    log.d('Eine Debug-Nachricht aus einer anderen Methode.');
   }
 }
